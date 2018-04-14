31d90a0155fa435c579a10259210da1615d60cc6
Brennan Cain <brenn.cain@gmail.com>
2018-04-14 13:52:33 -0400

    syntactically correct webpage

--- index.php
+++ index.php
@@ -4,65 +4,100 @@ Author:    Brennan Cain
 Email:     brennan@brennancain.com
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-	<!-- Description -->
+<head>
+  <?php
+  if(isset($_POST["project"]))
+  {
+    $_SESSION["project"] = $_POST["project"];
+  }
+  if(isset($_POST["slide"]))
+  {
+    $_SESSION["slide"] = $_POST["slide"];
+  }
+  ?>
+  <meta charset="UTF-8">
+  <!-- Description -->
 
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    </head>
-    <body>
-	<div class="row" style="height:100%; width: 100%">
-        	<div class="col-md-2" style="top: 0; bottom:0; overflow-y: scroll;">
-			<form action="contact.php" method="post">
-				<table>
-					<tr>
-						<td><textarea name="comment" required><?php if(isset($_SESSION["project"]) echo($_SESSION["project"]);?></textarea></td>
-						<td><input value="open" type='submit'></td>
-					</tr>
-				</table>
-		    </form>
-<?
-if(isset($_SESSION["project"]) {
-  dir = "snaps/" . $_SESSION["project"];
-	if (is_dir($dir)){
-    if ($dh = opendir($dir)){
-      $files = array();
-	    while (($file = readdir($dh)) !== false){
-	      array_push(files,$file)
-	    }
-	    closedir($dh);
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+  <div class="row" style="height:100%; width: 100%">
+    <div class="col-md-2" style="top: 0; bottom:0; overflow-y: scroll;">
+      <h1>Snapper</h1>
+      <form action="index.php" method="post">
+        <table>
+          <tr>
+            <td><textarea name="project" required><?php if(isset($_SESSION["project"])) {echo($_SESSION["project"]);}?></textarea></td>
+            <td><input value="open" type='submit'></td>
+          </tr>
+        </table>
+      </form>
+      <?php
+      if(isset($_SESSION["project"])) {
+        $dir = "snaps/" . $_SESSION["project"];
+        if (is_dir($dir)){
+          if ($dh = opendir($dir)){
+            $files = array();
+            while (($file = readdir($dh)) !== false){
+              array_push($files,$file);
+            }
+            closedir($dh);
 
-      // CREATE HTML HERE
-      echo("<ul>\n");
-      foreach($files as $file) {
-        if($file==$_SESSION["slide"]) {
-          echo("<li>$file</li>\n");
+            /*
+            $currentIndex=array_search($_SESSION["slide"]);//gets index of slide
+            echo("<div class='col-md-6'>");
+            if($currentIndex>0 and !$currentIndex){
+              echo "<span class="glyphicon glyphicon-align-left" aria-hidden="true"></span>"
+            }
+            */
+
+            echo("<ul>\n");
+            for($i=0; $i<count($files);$i++) {// CREATE LIST OF SLIDES HERE
+              $file=$files[$i];
+              if($file==$_SESSION["slide"]) {
+                echo("<li><strong>$file</strong></li>\n");
+              }
+              else {
+                echo("<li>$file</li>\n");
+              }
+            }
+            echo("<ul>\n");
+          }
+          else("Could not open directory");
         }
         else {
-          echo("<li><strong>$file</strong></li>\n");
+          echo("Directory not found<br>");
         }
       }
-      echo("<ul>\n");
-	  }
-    else("Could not open directory");
-	}
-  else {
-    echo("directory not found<br>");
-  }
-}
-?>
+      ?>
 
-		</div>
-        </div>
+    </div>
+    <div class="col-md-5" style="top: 0; bottom:0; overflow-y: scroll;">
+      <h2>Code</h2>
+      <hr>
+      <?php
+      if(isset($_SESSION["project"]) and isset($_SESSION["slide"])) {
+        include("snaps/" . $_SESSION["project"] . "/" . $_SESSION["slide"]."/code.html");
+      }
+      ?>
+    </div>
+    <div class="col-md-5" style="top: 0; bottom:0; overflow-y: scroll;">
+      <h2>Output</h2>
+      <hr>
+      <?php
+      if(isset($_SESSION["project"]) and isset($_SESSION["slide"])) {
+        include("snaps/" . $_SESSION["project"] . "/" . $_SESSION["slide"]."/output.html");
+      }
+      ?>
+    </div>
+  </div>
 
-    </body>
+</body>
 </html>
-
-+
