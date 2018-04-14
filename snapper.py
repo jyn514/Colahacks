3b2570713c59133553e3a4a078eba215742f060b
Joshua Nelson <jyn514@gmail.com>
2018-04-14 12:03:34 -0400

    add frame

---
+++ snapper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser
+
+def main(zipfile, source):
+    
+
+if __name__ == '__main__':
+    parser = ArgumentParser(__doc__)
+    parser.add_argument("zipfile")
+    parser.add_argument("source")
+    main(parser.parse_args())
