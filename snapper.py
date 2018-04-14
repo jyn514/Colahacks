66726594562834779e9639562b2874a74cbd00b7
Hunter Damron <hdamron1594@yahoo.com>
2018-04-14 13:35:21 -0400

    Added declarations for file_diff and add_strongs

--- snapper.py
+++ snapper.py
@@ -15,6 +15,24 @@ saveroot = root + '/snaps'
 compile_snap = ['./makesnap.sh']  # list of CLI args
 run_snap = ['./runsnap.sh']
 
+def file_diff(codefile, prevcodefile):
+    '''
+    Diffs the code file to previous code file then returns list of tuples with
+        lines which are changes
+    Parameters are both strings containing valid paths to code source
+    '''
+    with code, prevcode as open(codefile, "r"), open(prevcodefile, "r"):
+        pass #TODO(HD)
+
+def add_strongs(strong_lines, codehtmlfile):
+    '''
+    Parses html which is result of pygments to add html strong tags at specified
+        strong_lines
+    Parameter strong_lines is list of integer pairs which have [start, end)
+        line numbers starting from index 0
+    '''
+    pass #TODO(HD)
+
 def main(zipfile, source):
     '''(str, str) Accepts 2 file paths
     Returns nothing
@@ -30,15 +48,15 @@ def main(zipfile, source):
         os.makedirs(savedir)
 
     run(['unzip', '-d', zipdir, zipfile])  # unzip automatically adds extension
-    
+
     for version in os.listdir(zipdir):
         run(compile_snap)
         # decode required so string, not bytes
         output = run(run_snap, stdout=version).stdout.decode()
         pygments.highlight(output, BashLexer(), HtmlFormatter(), version + '/output.html')
         code = pygments.highlight(version, get_lexer_for_filename(source)(), HtmlFormatter(), version + '/code.html')
-        
-    
+
+
 
 if __name__ == '__main__':
     parser = ArgumentParser(__doc__)
