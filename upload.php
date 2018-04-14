84c3826bff00e01f361381ef81181ef8810c697e
Brennan Cain <brenn.cain@gmail.com>
2018-04-14 15:20:45 -0400

    fixed scrollable, added forward/backward, began upload

---
+++ upload.php
@@ -0,0 +1,7 @@
+<?php
+$filename = $_POST["filename"];
+$zipname = $_FILES["zip"]["name"];
+move_uploaded_files($_FILES["zip"]["tmp_name"],"zipfiles/$zipname");
+
+system("python snapper.py $zipname $filename")
+?>
