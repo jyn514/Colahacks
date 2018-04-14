3e25964a7543b15be157c0153ab16d1d138f517c
Joshua Nelson <jyn514@gmail.com>
2018-04-14 13:12:11 -0400

    update snapper

--- snapper.py
+++ snapper.py
@@ -6,31 +6,38 @@ from subprocess import run
 
 
 import pygments
-from pygments.lexers import shell
+from pygments.lexers.shell import BashLexer
 from pygments.formatters import HtmlFormatter
 
 root = '/var/www/html'
-ziproot = root + '/zipfiles'
+ziproot = root + '/zip'
+saveroot = root + '/snaps'
 compile_snap = ['./makesnap.sh']  # list of CLI args
 run_snap = ['./runsnap.sh']
 
 def main(zipfile, source):
-    '''(str, str) -> (str, str)
-    Accepts 2 file paths
-    Returns output, code as HTML string
+    '''(str, str) Accepts 2 file paths
+    Returns nothing
+    For each version of source, store code and output on disk
     '''
     zipfile = zipfile.replace('.zip', '')
     zipdir = ziproot + '/' + zipfile
+    savedir = saveroot + '/' + zipfile
+
     if not os.exists(zipdir):
         os.makedirs(zipdir)
+    if not os.exists(savedir):
+        os.makedirs(savedir)
+
     run(['unzip', '-d', zipdir, zipfile])  # unzip automatically adds extension
-    if not any(f.lower() == 'makefile' for f in os.listdir(zipdir)):
-        raise ValueError("Zip file must contain a makefile")
-    run(compile_snap)
-    # decode required so string, not bytes
-    output = run(run_snap, stdout=subprocess.PIPE).stdout.decode()
-    output = pygments.highlight(output, shell(), HtmlFormatter())
-    return output, code
+    
+    for version in os.listdir(zipdir):
+        run(compile_snap)
+        # decode required so string, not bytes
+        output = run(run_snap, stdout=version).stdout.decode()
+        pygments.highlight(output, BashLexer(), HtmlFormatter(), version + '/output.html')
+        code = pygments.highlight(version, get_lexer_for_filename(source)(), HtmlFormatter(), version + '/code.html')
+        
     
 
 if __name__ == '__main__':
