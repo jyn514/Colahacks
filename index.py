7e5b206f065d3f000bdeaf8833c902f5f1ff5a03
Joshua Nelson <jyn514@gmail.com>
2018-04-20 08:24:40 -0400

    this code does not work

--- index.py
+++ index.py
@@ -1,17 +1,34 @@
+import os
+
 from flask import Flask, render_template, app, request, session
+
 app = Flask(__name__)
+SNAP_DIR = 'snaps'
 
 @app.route("/", methods = ['GET', 'POST'])
 def main():
-    # if "clear" in request.args:
-    #     session.clear()
-    # project = request.values.get("project")
-    # if project is not None:
-    #     session["project"] = project
-    # slide = request.values.get("slide")
-    # if slide is not None:
-    #     session["slide"] = slide
+    if "clear" in request.args:
+        session.clear()
 
+    project = request.values.get("project")
+    if project is not None:
+        session['project'] = project
+        project = os.path.join(SNAP_DIR, project)
+        if os.path.isdir(project):
+            files = sorted(os.listdir(project))
+            session['files'] = files
+            slide = request.values.get("slide")
+
+            if slide is not None:
+                session['slide'] = slide
+                index = files.index(slide)
+            else:
+                session['slide'], index = files[0], 0
+
+            if index > 0:
+                session['prev_slide'] = files[index - 1]
+            if index + 1 < len(files):
+                session['next_slide'] = files[index + 1]
     return render_template("index.html", **session)
 
 if __name__ == "__main__":
